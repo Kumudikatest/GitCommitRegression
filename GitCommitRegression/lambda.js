559682e2a3395cd553f10f0410e04eb8fa5c3c69
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-22 09:06:45 +0530

    test

--- GitCommitRegression/lambda.js
+++ GitCommitRegression/lambda.js
@@ -5,7 +5,7 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
     sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/TestSQS`,
         AttributeNames: ['All'],
         MaxNumberOfMessages: '1',
         VisibilityTimeout: '30',
@@ -47,6 +47,16 @@ exports.handler = function (event, context, callback) {
                 else console.log(data);           // successful response
             });
         });
+    sqs.sendMessage({
+        MessageBody: 'test',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+        DelaySeconds: '0',
+        MessageAttributes: {}
+    }, function (data) {
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+        // your logic (logging etc) to handle failures, should be here
+    });
 
     callback(null, { "message": "Successfully executed" });
 }
